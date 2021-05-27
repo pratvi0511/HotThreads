231f5d6aaf08a8f1f9d378c0ff49cc3dd532fcbd
pratvi0511 <82817426+pratvi0511@users.noreply.github.com>
2021-05-27 18:07:35 +0530

    Add files via upload

---
+++ root/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" lang="en">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <meta name="generator" content="">  
+        <link rel="canonical" href="">
+
+        <title>hot threads</title>
+            <!-- Bootstrap core CSS -->
+        <link href="fontawesome/css/all.min.css" rel="stylesheet">
+        <link rel = 'stylesheet' href = "CSS/bootstrap.css">
+        <link rel ='stylesheet' href ='CSS/homePage.css'>
+        <link rel ='stylesheet' href ='CSS/header.css'>
+        <link rel ='stylesheet' href = 'CSS/f-style.css'>    
+        
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      .dropdown-item:hover{
+         color: #3989ca;
+         background-color: rgba(0, 0, 0, .85);
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    
+.checked {
+            color: orange;
+        }
+    </style>
+    </head>
+    <body>
+      <header>
+  <!-- Navbar start -->
+  <div class="scroll">
+    <nav class="navbar navbar-expand-md navbar-dark">
+
+      <a href="index.php">
+         <img  class ="image" src="images/hotthreadslogo.png"></a>
+     <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <div class="dropdown" data-toggle="dropdown">
+            <a class="nav-link active" href="pages/productPage.php">Collections</a>
+            <ul class="dropdown-menu" >
+              <a class="dropdown-item" href="#!">Men</a>
+              <a class="dropdown-item" href="#!">Women</a>
+            </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="pages/feedbackPage.php">Feedback</a>
+        </li>
+      </ul>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link active" href="pages/productPage.php">Products</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="pages/checkout.php"><i class="fas fa-money-check-alt mr-2"></i>Checkout</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="pages/cart.php"><i class="fas fa-shopping-cart"></i> Cart<span id="cart-item" class="badge badge-danger"  ></span></a>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link active" href="pages/loginPage.php">Sign In</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+  <!-- Navbar end -->
+  </header>
+  
+      <div class="container1">
+            <div class="background">
+                <div class="left-content">
+                    <div class="title">
+                        <div id="logo"> <img src="images/HotThreadsLogoTranparent.png" alt=""></div>
+                        <div id="titleline"><p>We are committed in giving<br> the latest fashion.</p></div>
+                        <div id="shopnow">
+                            <input type="button" class="button" value="Shop now">
+                        </div>
+                    </div>
+                </div>
+                <div class="right-content">
+                    <div id="mencollection1">
+                        <a href="#"><img src="images/menCollection1.png" alt="men collections demo"></a>
+                    </div>
+                    <div id="womencollection1">
+                        <a href="#"><img src="images/womencollection1.png" alt="women collection demo"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class = "About products">
+          <br><br><br>
+          <div class ="product1">
+              <div><img src ="images/denim.png"></div>
+              <div> <ul>
+                  <li style="font-size: 45px; text-align: center; font-family: Georgia;">FROM DUSK TO DAWN</li>
+                  <li> Comfort demin range collection to let you work effortlessly and shine with success.</li>
+              </ul></div>
+          </div>
+          <div class ="product2">
+            <div> <ul>
+                  <li style="font-size: 45px; text-align: center; font-family: Georgia;">WEEKENDS TO LOVE</li>
+                  <li> From my beach dates to netflix binge watching,<br>bagging in the coluttes collection to pump up some shades around you.</li>
+              </ul></div>
+              <div><img src ="images/culotte.png"></div>
+          </div>
+      </div>
+      <div class ="product3">
+          <div><img src ="images/formals.png"></div>
+          <div> <ul>
+                <li style="font-size: 45px; text-align: center; font-family: Georgia;">SLAY THE <br>OFFICE HOURS</li>
+                <li> We have tendency to throw our office clothes cauase <b>THEY ARE SO UNCOMFORTABLE.</b><br>But not anymore, pumping you up with our formals collection.</li>
+            </ul></div>
+           
+        </div>
+    </div>
+      <div class = "icons">
+          <div>
+              <div>
+              <img src = "images/cotton.png" width="20%" height ="70%">
+              </div>
+              <div>
+                  <h3>Premium Qaulity Fabric</h3>
+              </div>
+          </div>
+          <div>
+              <div>
+              <img src = "images/24-hours.png" width = "25%" height ="30%">
+              </div>
+              <div>
+                <h3> 24h Cancellation Policy</h3>
+              </div>
+          </div>
+          <div>
+              <div>
+              <img src = "images/delivery.png" width = "25%" height = "15%">
+              </div>
+              <div>
+                  <h3>6-7 day delivery</h3>
+              </div>
+          </div>
+          <div>
+              <div>
+                  <img src = "images/MII.jpg" width = "40%" height = "30%">
+               </div>
+               <div>
+                   <h3> Make In India</h3>
+               </div>
+          </div>
+          <div>
+              <div><img src = "images/price.png" width ="25%" height = "5%">
+              </div>
+              <div>
+                  <h3> Affordable Prices </h3>
+              </div>
+          </div>
+      </div>
+      <div>
+      
+      </div>
+      <div>
+          <div class ="About Us" style="padding-bottom: 15px; padding-top: 15px;"><h1 style="text-align: center;"> Wanna Know About Us ?</h1></div>
+          
+            <div class = "gifc">
+              <div class="pic1">
+                <img src = "images/cotton.gif" width = "40%" height = "700px">
+              </div>
+              <div class = "work">
+              <ul>
+                  <li><h3>Fabric</h3><p>Plucked right from the cotton farms of India and manufactured under the guidance of skilled professionals,
+                  having more than 30+ years of experience in the field of textile manufacturing. Bringing,best quality at your doorstep.</p></li>
+                  <li><h3>Color Fastness</h3><p> Made with premium qaulity of dyes and modern technicque of chromatography, the colors remain intact within the threads for 
+                  years to come.</p></li>
+                  <li><h3>Fine Stitching</h3><p> Threads of cloth woven together in the intricate binding with a dual locking techniques, that fastens your relation and trust on this 
+                  brand for generations.</p></li>
+                  <li><h3>Make in India</h3><p>This is a home grown brand that manufactures and sells pan India. Following the tagline penned <b>"ZERO DEFECT ZERO EFFECT"</b> the brand
+                  is on the journey to be the part of <b>"AATMANIRBHAR BHARAT".</b> Let us all support a local brand together.</p></li>
+              </ul>
+          </div>
+      </div>
+      </div>
+
+          <footer>
+    <div class="up-section">
+      <ul>
+        <h1>Top Categories</h1>
+        <li><a href="pages/productPage.php">Jeans</a></li>
+        <li><a href="pages/productPage.php">Top</a></li>
+      </ul>
+      <ul>
+        <h1>Information & Customer Service</h1>
+        <li><a href="pages/AboutUs.php">About us</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Terms of Use</a></li>
+        <li><a href="pages/ReturnsCancellation.php">Returns & Cancellation</a></li>
+        <li><a href="#">FAQ</a></li>
+      </ul>        
+      <ul>
+        <h1>My Account</h1>
+        <li><a href="#">My Account</a></li>
+        <li><a href="#">Order History</a></li>
+        <li><a href="#">Track Order</a></li>
+        <li><a href="#">Wish List</a></li>
+      </ul>
+      <ul>
+        <h1>Contact Us</h1>
+
+        <li><p>HotThreads@gmail.com</p></li>
+        <li><p>+91 6354339383</p></li>
+      </ul>
+      
+      <div class="follow">
+        <h1>Follow Us</h1>
+        <div class="follow-icon">
+          <a href="https://www.facebook.com/Hot-Threads-113552533795196/"><i class="fab fa-facebook-f"></i></a>
+          <a href="https://www.instagram.com/invites/contact/?i=1llwsis1okxaz&utm_content=kb01cdy" target="_blank"><i class="fab fa-instagram"></i></a>
+          <a href="https://wa.me/c/916354339383"><i class="fab fa-whatsapp"></i></a>
+        </div>
+      </div>
+    </div>
+    <p class="copyright" >Going to Internet copyright 2009</p>
+  </footer>
+          <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')</script>
+      <script src="bootstrap.bundle.min.js"></script>
+      <script src="bootstrap.min.js"></script>
+      <script src="jquery.min.js"></script>
+    </body>
+</html>
